872ecca7e3a956358788b51f88b5acfac7202d57
the_tk_official <the.tk.official@gmail.com>
2021-10-08 10:50:49 +0300

    Python Print and Input Function

---
+++ Exercise 3.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = str(input("Which city did u grow up in?\n"))
+pet = str(input("What is the name of a pet?\n"))
+print("Your band name could be " + city + " " + pet)
